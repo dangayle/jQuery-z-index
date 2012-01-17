bda230deb7bfc3cc4a66c1dc5a04db49ffc9c1fa
Dan Gayle <dangayle@gmail.com>
2012-01-16 19:27:47 -0800

    Initial commit
    Added to repository

---
+++ jquery.zindex.js
@@ -0,0 +1,24 @@
+//Thanks to lonesomeday at stackoverflow http://stackoverflow.com/a/8568191/250241
+
+(function($) {
+    var highest = 1;
+    var publicMethods = {
+        up : function() {
+            this.css('z-index', ++highest); // increase highest by 1 and set the style
+            return this;
+        },
+        down : function(){
+            this.css('z-index', --highest); // increase highest by 1 and set the style
+            return this;
+        }
+    }
+    $.fn.zindex = function(method){
+        if (publicMethods[method]){
+            return publicMethods[method].apply(this);
+        }
+        else{
+            $.error( 'Method ' +  method + ' does not exist. Yo.' );
+        }
+    }
+
+})(jQuery);
